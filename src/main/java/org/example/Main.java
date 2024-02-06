31a806885b9ec925254cd06bdd34535d82896457
a3rzi <amirhosseinqatar@gmail.com>
2024-02-06 17:42:14 +0330

    length/split()/StringBuilder/reverse()/ toString()

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,30 @@
 package org.example;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //given
+        String sentence = "Hey fellow warriors abcde abcd";
+
+        //when
+        String result = spinWords(sentence);
+        System.out.println(result);
+
+    }
+
+    private static String spinWords(String sentence) {
+
+        String[] words = sentence.split(" ");
+
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].length()>=5){
+                words[i] = reverseWord(words[i]);
+            }
+        }
+        return String.join(" ", words);
+    }
+
+    private static String reverseWord(String word) {
+        StringBuilder str = new StringBuilder(word);
+        str.reverse();
+        return str.toString();
     }
 }
---
+++ src/test/java/FunctionTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class FunctionTest {
+    @Test
+    void lengthTest() {
+        String str = "amir";
+        System.out.println(str.length());
+    }
+}
